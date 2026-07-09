de2e400e3ae3b0c04cc733706f28d2866ad1dcfe
Maxim Seleznev <maximuhkass14@gmail.com>
2026-07-09 13:56:31 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-wrap content-start h-screen w-screen gap-10">
+        <div className="w-50 h-32 bg-red-500" />
+        <div className="w-50 h-32 bg-blue-500" />
+        <div className="w-50 h-32 bg-green-500" />
+        <div className="w-50 h-32 bg-yellow-500" />
+      </div>
+    </main>
+  );
+}
